18c536447599c8d8821066f9fcbee2620b1043dc
Soykot Alam <a.k.mshahalam68@gmail.com>
2023-02-19 23:40:10 +0600

    index.js

--- js/index.js
+++ js/index.js
@@ -1,2 +1,8 @@
-const user = {id:11, name:'Gorib Amir', job: 'actor'}
+// JavaScript Object Notation
+// JASON
+
+
+const user = {id:11, name:'Gorib Amir', job: 'actor'};
+const stringified = JSON.stringify(user);
 console.log(user);
+console.log(stringified);
