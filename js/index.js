e34b87a9d45e7298fe03c42cc4c6ae1dbc5eaa43
Soykot Alam <a.k.mshahalam68@gmail.com>
2023-02-19 23:50:59 +0600

    index.js

--- js/index.js
+++ js/index.js
@@ -4,5 +4,15 @@
 
 const user = {id:11, name:'Gorib Amir', job: 'actor'};
 const stringified = JSON.stringify(user);
-console.log(user);
-console.log(stringified);
+// console.log(user);
+// console.log(stringified);
+
+const shop ={
+    name: 'Alia Store',
+    address: 'Ranbir road',
+    product:['laptop','mobile','pepsi'],
+    isExpensive: false
+}
+const shopStringified = JSON.stringify(shop);
+console.log(shop);
+console.log(shopStringified);
