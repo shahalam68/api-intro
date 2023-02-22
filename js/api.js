ec6ae61b6ca533beaf1bb06d392f1a8aa485ef11
Soykot Alam <a.k.mshahalam68@gmail.com>
2023-02-22 23:13:07 +0600

    fetch

--- js/api.js
+++ js/api.js
@@ -4,3 +4,17 @@ function loadData(){
     .then(res => res.json())
     .then(json => console.log(json))
 }
+
+function loadUsers(){
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then(res => res.json())
+    .then(data => displayUsers(data))
+}
+function loadPosts(){
+    fetch("https://jsonplaceholder.typicode.com/posts")
+    .then(res => res.json())
+    .then(data => console.log(data))
+}
+function displayUsers(data){
+    console.log(data)
+}
